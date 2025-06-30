08176effae1d85b57c389770cb57b000ef413a50
rutvikmevada08 <rutvikmevada08@gmail.com>
2025-06-30 21:41:11 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number the number:");
+    scanf("%d",&n);
+    // printf("Enter the number the number 2:");
+    // scanf("%d",&b);
+   printf("The sqyare if num is :%d",n*n);
+    return 0;
+}
