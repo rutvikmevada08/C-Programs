47baec137a233d1caa7bb779630b11202431e5c6
rutvikmevada08 <rutvikmevada08@gmail.com>
2025-07-07 10:12:06 +0530

    Create TemperatureConverter.c

---
+++ TemperatureConverter.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float temp, converted;
+
+    printf("Temperature Converter\n");
+    printf("1. Celsius to Fahrenheit\n");
+    printf("2. Fahrenheit to Celsius\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        printf("Enter temperature in Celsius: ");
+        scanf("%f", &temp);
+        converted = (temp * 9 / 5) + 32;
+        printf("Temperature in Fahrenheit: %.2f\n", converted);
+    } 
+    else if (choice == 2) {
+        printf("Enter temperature in Fahrenheit: ");
+        scanf("%f", &temp);
+        converted = (temp - 32) * 5 / 9;
+        printf("Temperature in Celsius: %.2f\n", converted);
+    } 
+    else {
+        printf("Invalid choice.\n");
+    }
+
+    return 0;
+}
